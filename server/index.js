ff21491f43d6b669691bcb4fb4d3fe8634dda868
ujjwal gupta <ujjawalgupta0274@gmail.com>
2025-07-13 22:02:33 +0530

    � Add client build script for Render

--- server/index.js
+++ server/index.js
@@ -41,10 +41,10 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/dist")));
+  app.use(express.static(path.join(__dirname, "client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
